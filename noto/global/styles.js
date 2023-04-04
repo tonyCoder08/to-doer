90dea69407c63f572589f8c365fe5d74edbbf4b6
maddyCoder <maddyyyyyyy6@gmail.com>
2023-04-04 06:01:03 +0000

    globalstyle add

---
+++ noto/global/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const globalstyles = StyleSheet.create({
+    infoText: {
+        fontFamily: "Inter_400Regular",
+        fontSize: 18,
+        alignSelf: "center",
+        justifyContent: "center",
+        textAlign: "center",
+        color:"#687076",
+        marginTop:18
+      },
+})
